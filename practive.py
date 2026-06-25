573a428cae8f0bdb22f185489fc862279de282ce
Aashish Kar <aashishkarmacharya79@gmail.com>
2026-06-25 10:46:33 +0545

    revise

--- practive.py
+++ practive.py
@@ -12,11 +12,36 @@ student ={
 
 print(student["name"])
 
-#if-else
-number = int(input("Enter a number = "))
-if number > 0:
-    print("positive")
-elif number < 0:
-    print("Negative")
-else:
-    print("Zero")
+#while loop
+i= 1
+while i < 11:
+    print(i)
+    i = i + 1
+
+
+#for loop
+for num in range(2,20,2):
+    print("even numbers = ", num)
+
+
+#function
+def add_numbers(a,b):
+    return a + b
+
+result = add_numbers(10,10)
+print(result)
+
+#list
+list = [1,2,3,4,5]
+print(list[0])
+print(list[-1])
+print(len(list))
+
+#loop through list
+fruits =["apple","banana","mango","pineapple","grape"]
+for fruit in fruits:
+    print(fruit)
+
+#range
+for i in range(10,0,-1):
+    print(i)
