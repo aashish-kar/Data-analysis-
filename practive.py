d527cc699432e1f2b633aaf7be9c22fe9cf39085
Aashish Kar <aashishkarmacharya79@gmail.com>
2026-06-25 09:05:47 +0545

    practice

---
+++ practive.py
@@ -0,0 +1,22 @@
+#set
+fruits ={"apple","banana","mango","pineapple","grape"}
+fruits.add("watermelon")
+print(fruits)
+
+#dictionary
+student ={
+    "name":"aashish",
+    "age" : 21,
+    "faculty" : "Data Analysis"
+}
+
+print(student["name"])
+
+#if-else
+number = int(input("Enter a number = "))
+if number > 0:
+    print("positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
